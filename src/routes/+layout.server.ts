dcfa6158d9cd7bacbb00f438741a77ad47876e16
cpx <12119342+carpedil@users.noreply.github.com>
2023-06-11 11:14:17 +0800

    change titile

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const load = async () => {
-	let title = 'OSF Server Api A/B Test Function';
+	let title = 'DREAMS Server Api Test ';
 	const modules = await prisma.module.findMany();
 
 	return {
